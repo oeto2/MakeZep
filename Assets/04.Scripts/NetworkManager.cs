165a112fef508c8c0dbc1f40a4341ff87d0b86ea
oeto2 <oeto2@naver.com>
2024-01-18 15:38:08 +0900

    240118_Add Player Animation

--- Assets/04.Scripts/NetworkManager.cs
+++ Assets/04.Scripts/NetworkManager.cs
@@ -1,8 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class NetworkManager : MonoBehaviour
+
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    
+    public InputField NickNameInput;
+    public GameObject ConnectPanel; //접속 창
+
+
+    private void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    //접속
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null); //총 인원수 6명
+    }
+
+    //방 접속시
+    public override void OnJoinedRoom() 
+    {
+        ConnectPanel.SetActive(false); //접속창 비활성화
+    }
+
+    //Esc키 입력시 접속 끊기
+    private void Update() { if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected) PhotonNetwork.Disconnect(); }
+
+    //접속이 끊길 경우
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        ConnectPanel.SetActive(true); //접속창 활성화
+    }
 }
