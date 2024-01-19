5fe886ba40f1f8caefdd1cf754d8f2932b2bdc7e
oeto2 <oeto2@naver.com>
2024-01-19 09:41:04 +0900

    240119_Add Character Select Panel

---
+++ Assets/04.Scripts/UIPanel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+    public GameObject ReNamePanel;
+    public GameObject SelectChPanel;
+
+    public void ShowReNamePanel() { if (!SelectChPanel.activeSelf) ReNamePanel.SetActive(true); }
+    public void ShowSelectChPanel() { if (!ReNamePanel.activeSelf) SelectChPanel.SetActive(true); }
+}
