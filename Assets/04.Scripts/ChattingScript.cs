a89e522f94d4b66b1ba6549fa01bc71236370f05
oeto2 <oeto2@naver.com>
2024-01-21 22:38:42 +0900

    240121_Edit Chatting System

--- Assets/04.Scripts/ChattingScript.cs
+++ Assets/04.Scripts/ChattingScript.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class ChattingScript : MonoBehaviour
+public class ChattingScript : MonoBehaviourPunCallbacks
 {
     public InputField ChattingInputField;
     public Text[] ChatText;
 
+    public void InputChattingRPC() => photonView.RPC("InputEnter", RpcTarget.AllBuffered);
+
+    [PunRPC]
     public void InputEnter()
     {
         bool isFull = true;
 
         for (int i = 0; i < ChatText.Length; i++)
-        { 
-            if(ChatText[i].text == "")
+        {
+            if (ChatText[i].text == "")
             {
                 isFull = false;
                 ChatText[i].text = ChattingInputField.text;
@@ -22,13 +27,13 @@ public class ChattingScript : MonoBehaviour
             }
         }
 
-        if(isFull)
+        if (isFull)
         {
-            for(int i = 1; i< ChatText.Length; i++)
+            for (int i = 1; i < ChatText.Length; i++)
             {
                 ChatText[i - 1].text = ChatText[i].text;
-                
-                if(i == ChatText.Length -1)
+
+                if (i == ChatText.Length - 1)
                 {
                     ChatText[i].text = ChattingInputField.text;
                 }
