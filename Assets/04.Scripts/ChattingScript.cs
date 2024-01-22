cd2a825952857e29bb82f36c9fb38dc8fe8e6243
oeto2 <oeto2@naver.com>
2024-01-22 15:41:59 +0900

    240122_Add Chatting System

--- Assets/04.Scripts/ChattingScript.cs
+++ Assets/04.Scripts/ChattingScript.cs
@@ -8,39 +8,45 @@ using Photon.Realtime;
 public class ChattingScript : MonoBehaviourPunCallbacks
 {
     public PhotonView ChattingPhotonView;
-    public InputField ChattingInputField;
-    public Text[] ChatText;
 
-    public void InputChattingRPC() => ChattingPhotonView.RPC("InputEnter", RpcTarget.AllBuffered);
+    public InputField ChattingInputField;
+    //public Text[] ChatText;
+    public List<Text> ChatText;
+
+    public GameObject TextPrefab;
+
+    //public void InputChattingRPC() => ChattingPhotonView.RPC("SendButtonClicked", RpcTarget.All);
+
+    //채팅 입력값 넘겨주기
+    public void SendButtonClicked()
+    {
+        //텍스트 입력값과 플레이어 이름 넘겨주고 RPC
+        ChattingPhotonView.RPC("PlayerChatting", RpcTarget.All, "<color=#00FF18>" + PhotonNetwork.LocalPlayer.NickName + ": "
+            + "</color>" + ChattingInputField.text);
+    }
 
     [PunRPC]
-    public void InputEnter()
+    //넘겨 받은 텍스트 값으로 UI Text에 적용
+    public void PlayerChatting(string _inputText)
     {
-        bool isFull = true;
+        GameObject Content = this.transform.Find("Scroll View/Viewport/Content").gameObject;
 
-        for (int i = 0; i < ChatText.Length; i++)
+        //텍스트 오브젝트 Prefab 생성 후 Content 자식으로 할당
+        GameObject TextObject = Instantiate(TextPrefab, Content.transform.position, Quaternion.identity);
+        TextObject.transform.SetParent(Content.transform);
+        TextObject.transform.localScale = new Vector3(1, 1, 1);
+
+        //ChatText List에 TextObject 추가
+        ChatText.Add(TextObject.GetComponent<Text>());
+
+        for (int i = 0; i < ChatText.Count; i++)
         {
             if (ChatText[i].text == "")
             {
-                isFull = false;
-                ChatText[i].text = ChattingInputField.text;
+                ChatText[i].text = _inputText;
                 break;
             }
         }
-
-        if (isFull)
-        {
-            for (int i = 1; i < ChatText.Length; i++)
-            {
-                ChatText[i - 1].text = ChatText[i].text;
-
-                if (i == ChatText.Length - 1)
-                {
-                    ChatText[i].text = ChattingInputField.text;
-                }
-            }
-        }
-
         ChattingInputField.text = "";
     }
 }
